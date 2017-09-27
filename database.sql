7005084b7963fc6bda0d3aa31296d97612c980ac
root <root@autoxit.ru>
2017-09-27 19:37:45 +0300

    database.sql dumped with --routines flag

--- database.sql
+++ database.sql
@@ -175,6 +175,307 @@ LOCK TABLES `user` WRITE;
 INSERT INTO `user` VALUES (1,'test','db2d303c20b9468bbe90114d3d1874b3','2017-09-27 07:19:51');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'test3'
+--
+/*!50003 DROP FUNCTION IF EXISTS `fi_account_history` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_UNSIGNED_SUBTRACTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`%` FUNCTION `fi_account_history`(
+	`in_account_id` BIGINT( 22 ) UNSIGNED ,
+	`in_amount` NUMERIC( 10 , 2 ) UNSIGNED ,
+    `in_array` TINYINT( 1 ) UNSIGNED
+) RETURNS bigint(22) unsigned
+    COMMENT 'Создание транзакции по счёту'
+BEGIN
+	DECLARE `v_amount` NUMERIC( 10 , 2 ) UNSIGNED DEFAULT (
+		SELECT SQL_SMALL_RESULT
+			`a1`.`amount`
+		FROM
+			`account` AS `a1`
+		WHERE
+			( `a1`.`id` = `in_account_id` )
+		LIMIT 1
+    ) ;
+
+	IF ( `v_amount` IS null ) THEN
+		RETURN null ;
+    END IF ;
+
+    IF ( ( `in_array` IS false ) AND ( `v_amount` < `in_amount` ) ) THEN
+		RETURN null ;
+    END IF ;
+
+	INSERT INTO
+		`account_history`
+	SET
+		`account_id` = `in_account_id` ,
+		`amount` = `in_amount` ,
+		`array` = `in_array` ;
+
+	RETURN last_insert_id( ) ;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP FUNCTION IF EXISTS `fi_session` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_UNSIGNED_SUBTRACTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`%` FUNCTION `fi_session`(
+	`in_user_id` BIGINT( 22 ) UNSIGNED ,
+    `in_expires` BIGINT( 22 ) UNSIGNED
+) RETURNS char(32) CHARSET utf8
+    COMMENT 'Создание сессии'
+BEGIN
+	DECLARE `v_expires` DATETIME DEFAULT from_unixtime( unix_timestamp( ) + `in_expires` ) ;
+
+	UPDATE
+		`session` AS `s1`
+	SET
+		`s1`.`expires` = `v_expires`
+    WHERE
+		( `s1`.`user_id` = `in_user_id` ) AND
+        ( `s1`.`expires` >= current_timestamp( ) ) ;
+
+    IF ( NOT row_count( ) ) THEN
+		INSERT INTO
+			`session`(
+				`id` ,
+				`user_id` ,
+				`expires`
+			)
+		SELECT
+			md5( uuid( ) ) AS `session_id` ,
+			`u1`.`id` AS `user_id` ,
+			`v_expires` AS `expires`
+		FROM
+			`user` AS `u1`
+		WHERE
+			( `u1`.`id` = `in_user_id` )
+		LIMIT 1 ;
+    END IF ;
+
+	RETURN `fs_session`( `in_user_id` ) ;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP FUNCTION IF EXISTS `fi_user` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_UNSIGNED_SUBTRACTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`%` FUNCTION `fi_user`(
+	`in_login` VARCHAR( 20 ) ,
+    `in_passwd` VARCHAR( 20 )
+) RETURNS bigint(22) unsigned
+    COMMENT 'Создание пользователя'
+BEGIN
+	INSERT INTO
+		`user`
+	SET
+		`login` = `in_login` ,
+        `passwd` = `fs_passwd`( `in_login` , `in_passwd` ) ;
+
+	RETURN last_insert_id( ) ;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP FUNCTION IF EXISTS `fs_account_history_balance` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_UNSIGNED_SUBTRACTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`%` FUNCTION `fs_account_history_balance`(
+	`in_account_id` BIGINT( 22 ) UNSIGNED
+) RETURNS decimal(10,2)
+    COMMENT 'Баланс счёта'
+BEGIN
+	RETURN
+		`fs_account_history_sum`( `in_account_id` , true ) -
+        `fs_account_history_sum`( `in_account_id` , false ) ;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP FUNCTION IF EXISTS `fs_account_history_sum` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_UNSIGNED_SUBTRACTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`%` FUNCTION `fs_account_history_sum`(
+	`in_account_id` BIGINT( 22 ) UNSIGNED ,
+    `in_array` TINYINT( 1 ) UNSIGNED
+) RETURNS decimal(10,2) unsigned
+    COMMENT 'Сумма счёта под дебиту(0) или по кредиту(1)'
+BEGIN
+	DECLARE `v_result` NUMERIC( 10 , 2 ) UNSIGNED DEFAULT (
+		SELECT SQL_SMALL_RESULT
+			sum( `ah1`.`amount` ) AS `debit`
+        FROM
+			`account_history` AS `ah1`
+		WHERE
+			( `ah1`.`account_id` = `in_account_id` ) AND
+            ( `ah1`.`array` = `in_array` )    
+    ) ;
+
+	RETURN coalesce( `v_result` , 0.0 ) ;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP FUNCTION IF EXISTS `fs_passwd` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_UNSIGNED_SUBTRACTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`%` FUNCTION `fs_passwd`(
+	`in_login` VARCHAR( 20 ) ,
+    `in_passwd` VARCHAR( 20 )
+) RETURNS char(32) CHARSET utf8
+    COMMENT 'Хэш логина и пароля'
+BEGIN
+	RETURN md5( concat( md5( `in_login` ) , md5( `in_passwd` ) ) ) ;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP FUNCTION IF EXISTS `fs_session` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_UNSIGNED_SUBTRACTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`%` FUNCTION `fs_session`(
+	`in_user_id` BIGINT( 22 ) UNSIGNED
+) RETURNS char(32) CHARSET utf8
+    COMMENT 'Идентификатор сессии'
+BEGIN
+	RETURN (
+		SELECT
+			`s1`.`id` AS `session_id`
+		FROM
+			`session` AS `s1`
+		WHERE
+			( `s1`.`user_id` = `in_user_id` ) AND
+			( `s1`.`expires` > current_timestamp( ) )
+		LIMIT 1
+	) ;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP FUNCTION IF EXISTS `fs_user` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_UNSIGNED_SUBTRACTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`%` FUNCTION `fs_user`(
+	`in_login` VARCHAR( 20 ) ,
+    `in_passwd` VARCHAR( 20 )
+) RETURNS bigint(22) unsigned
+    COMMENT 'Идентификатор пользователя по логину и паролю'
+BEGIN
+	RETURN (
+		SELECT
+			`u1`.`id` AS `user_id`
+        FROM
+			`user` AS `u1`
+		WHERE
+            ( `u1`.`passwd` = `fs_passwd`( `in_login` , `in_passwd` ) )
+    ) ;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `pu_account_amount` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_UNSIGNED_SUBTRACTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`%` PROCEDURE `pu_account_amount`(
+	`in_account_id` BIGINT( 22 ) UNSIGNED
+)
+    COMMENT 'Поддержание актуальности поля `amount`'
+BEGIN
+	UPDATE
+		`account` AS `a1`
+	SET
+		`a1`.`amount` = `fs_account_history_balance`( `in_account_id` )
+	WHERE
+		( `a1`.`id` = `in_account_id` ) ;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -185,4 +486,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-09-27 18:09:15
+-- Dump completed on 2017-09-27 19:37:40
