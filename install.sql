e71e23ddb5366fbbf3ecdb2b04ddeab9a3eb2732
root <root@autoxit.ru>
2017-09-28 16:18:30 +0300

    User name from 'test' to 'test3' fixed

--- install.sql
+++ install.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE `test3` DEFAULT CHARACTER SET utf8 ;
-CREATE USER `test`@`localhost` IDENTIFIED BY 'test' ;
+CREATE USER `test3`@`localhost` IDENTIFIED BY 'test3' ;
 GRANT ALL PRIVILEGES ON `test3`.* TO `test3`@`localhost` ;
 FLUSH PRIVILEGES ;
 USE `test3` ;
